cdf02f4ab736756290f48013731014630e2da405
Christoph Holtermann <mail@c-holtermann.net>
2021-11-15 01:29:03 +0100

    use configfile and command line arguments

--- plot_statistics.py
+++ plot_statistics.py
@@ -29,10 +29,46 @@ today = timenow_date_str = str(timenow.year) + "-" + \
                 str(timenow.month) + "-" + \
                 str(timenow.day)
 
-def get_infiles():
 
-    _glob = timenow_date_str + "_statistics_*.csv"
-    glob_full = os.path.join(datadir, _glob)
+
+def readCommandLine():
+    # Create the parser
+    parser = argparse.ArgumentParser()
+
+    # Add argument datadir
+    parser.add_argument('--datadir', type=pathlib.Path, default=datadir)
+
+    # Add argument imagedir
+    parser.add_argument('--imagedir', type=pathlib.Path, default=imgdir)
+
+    # Add argument configfile
+    parser.add_argument('--configfile', type=argparse.FileType('r'), default=configFileName)
+
+    # Add argument verbose
+    parser.add_argument('--verbose', type=bool, default=False)
+
+    # Add argument verbose
+    parser.add_argument('--showplot', type=bool, default=False)
+
+    # Parse the argument
+    args = parser.parse_args()
+
+    return args
+
+def readConfigFile(args):
+    configFile = args.configfile
+    data = json.load(configFile)
+
+    return data
+
+def get_infiles(args, config):
+
+    if config["filename"]:
+        _glob = config["filename"].format(today = today)
+    else:
+        _glob = today + "_statistics"
+    _glob += "_*.csv"
+    glob_full = os.path.join(args.datadir, _glob)
 
     files = glob.glob(glob_full)
 
@@ -69,12 +105,26 @@ def read_infiles(files):
     return plot_datas
 
 def main():
-    files = get_infiles()
+    global config, args, files, plot_datas
+
+    args = readCommandLine()
+    config = readConfigFile(args)
+
+    files = get_infiles(args, config)
+
+    if not files:
+        print("no data files found")
+        return
 
     print("plotting files", files)
 
-    outfile_img_name = timenow_date_str + "_fig"
-    outfile_img_name_full = os.path.join(imgdir, outfile_img_name)
+    today = timenow_date_str
+
+    if config["filename"]:
+        outfile_img_name = config["filename"].format(today=today)
+    else:
+        outfile_img_name = today + "_fig"
+    outfile_img_name_full = os.path.join(args.imagedir, outfile_img_name)
 
     # files=["statistics_1.csv","statistics_2.csv"]
 
@@ -132,8 +182,18 @@ def main():
 
     #fig_legend = ax.legend(legpl,label_list,loc=2, bbox_to_anchor=(.05,-0.1), borderaxespad=0.)
 
-    ax_base.set_title("Summe auf Pubmed gelisteter Publikationen")
-    ax_base.set_xlabel("Jahr")
+    if config["title"]:
+        title = config["title"]
+    else:
+        title = "Summe auf Pubmed gelisteter Publikationen"
+
+    if config["xlabel"]:
+        xlabel = config["xlabel"]
+    else:
+        xlabel = "Jahr"
+
+    ax_base.set_title(title)
+    ax_base.set_xlabel(xlabel)
     #ax_base.set_ylabel("Summe Publikationen Ant. Med.")
     #ax.set_ylabel("Summe Publikationen Pubmed")
 
@@ -147,8 +207,9 @@ def main():
     fig.savefig(outfile_img_name_full, #bbox_extra_artists=(fig_legend,),
                 bbox_inches='tight', pad_inches=0.2)
 
-    plt.draw()
-    plt.show()
+    if args.showplot:
+        plt.draw()
+        plt.show()
 
 if __name__ == "__main__":
     main()
